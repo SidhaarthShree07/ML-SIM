91761209a4c8213b7a3cad0534d4474cadeb8ae8
Sidhaarth Shree <127885925+SidhaarthShree07@users.noreply.github.com>
2025-04-15 08:46:59 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -7,6 +7,8 @@ import requests
 import re
 import os
 
+
+
 app = Flask(__name__)
 
 # Load pre-cleaned dataset
@@ -130,4 +132,5 @@ def chatbot():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
